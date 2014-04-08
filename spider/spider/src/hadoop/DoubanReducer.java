b6e1c691874d759fdb58946256f6d0136d986088
duanfa <duanfa@xinnet.com>
2014-04-08 10:39:52 +0800

    aaaaaaaaaaaaa

--- spider/spider/src/hadoop/DoubanReducer.java
+++ spider/spider/src/hadoop/DoubanReducer.java
@@ -5,15 +5,14 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class DoubanReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-	private IntWritable result = new IntWritable();
+public class DoubanReducer extends Reducer<Text, Text, Text, Text> {
 
 	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-		int sum = 0;
+		/*int sum = 0;
 		for (IntWritable val : values) {
 			sum += val.get();
 		}
 		result.set(sum);
-		context.write(key, result);
+		context.write(key, result);*/
 	}
 }
